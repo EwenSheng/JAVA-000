e82491a48b0cff2b2cd92859b22560e021748849
EwenSheng <453999757@qq.com>
2020-11-17 18:18:19 +0800

    Update job progress
    1. 完成jdbc作业
    2. 完成PrepareStatement
    3. 修改md

--- Week_05/fifth-week-home-work-spring-boot/src/main/java/org/home/work/controller/DemoController.java
+++ Week_05/fifth-week-home-work-spring-boot/src/main/java/org/home/work/controller/DemoController.java
@@ -1,10 +1,13 @@
 package org.home.work.controller;
 
+import org.home.work.entity.vo.AccountBatchIdUpdateVO;
+import org.home.work.entity.vo.AccountSingleUpdateVO;
+import org.home.work.serivce.AccountService;
 import org.home.work.service.SchoolService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
 
 /**
  * @author: Ewen
@@ -19,11 +22,32 @@ public class DemoController {
     @Autowired
     private SchoolService schoolService;
 
+    @Autowired
+    private AccountService accountService;
+
     @GetMapping(value = "/print")
     public String print() {
 
         schoolService.doIt();
 
         return "success";
+    }
+
+    @GetMapping(value = "/get_account/{id}")
+    public String getAccount(@PathVariable(name = "id") Integer id) throws SQLException {
+
+        return accountService.getAccountById(id).toString();
+    }
+
+    @PostMapping(value = "/update_account")
+    public String updateAccountName(@RequestBody AccountSingleUpdateVO vo) throws SQLException {
+
+        return accountService.updateAccountName(vo.getId(), vo.getName()) ? "success" : "fail";
+    }
+
+    @PostMapping(value = "/update_batch_account")
+    public String updateBatchAccountName(@RequestBody AccountBatchIdUpdateVO vo) throws SQLException {
+
+        return accountService.updateBatchAccountByIds(vo.getIds()) ? "success" : "fail";
     }
 }
