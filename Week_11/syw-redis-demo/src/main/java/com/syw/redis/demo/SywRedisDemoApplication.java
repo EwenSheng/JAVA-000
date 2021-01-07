35dd0ce1348ba5b1baff20aebc208c6603e8ccfd
EwenSheng <453999757@qq.com>
2021-01-07 20:37:40 +0800

    add

---
+++ Week_11/syw-redis-demo/src/main/java/com/syw/redis/demo/SywRedisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.syw.redis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SywRedisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SywRedisDemoApplication.class, args);
+    }
+
+}
